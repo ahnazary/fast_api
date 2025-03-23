03e656652e624b05547351543fbe7a9e2987e176
ahnazary <ah.nazary.aghchemazary@gmail.com>
2025-03-23 19:39:31 +0100

    Get the hash password from the postgres table

--- banking_api/src/routes/auth.py
+++ banking_api/src/routes/auth.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from postgres_interface import get_db
+from postgres_models import Users
 from security import create_access_token, hash_password, verify_password
 from sqlalchemy.orm import Session
 
@@ -18,8 +19,8 @@ users_db = {
 def login_user(
     form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
 ):
-    user = users_db.get(form_data.username)
-    if not user or not verify_password(form_data.password, user["hashed_password"]):
+    user = db.query(Users).filter(Users.username == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid credentials",
