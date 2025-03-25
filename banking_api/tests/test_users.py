1f1e834096fce9d27e3cee4bdebbeb44e0b87df2
ahnazary <ah.nazary.aghchemazary@gmail.com>
2025-03-25 23:39:26 +0100

    Add new test for unauthorized user

--- banking_api/tests/test_users.py
+++ banking_api/tests/test_users.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
 
 import pytest
 from sqlalchemy import text
-from src.security import verify_password
+from src.security import create_access_token, verify_password
 
 
 @pytest.mark.parametrize(
@@ -31,3 +31,19 @@ def test_create_new_user(
     assert user is not None
     assert user[0] == username
     assert verify_password(password, user[1])
+
+
+def test_create_new_user_unauthorized(test_client, test_db):
+    jwt_token = create_access_token(data={"sub": "unauthorized_user"})
+    response = test_client.post(
+        "/users/create/",
+        json={"username": "test_user_3", "password": "test123"},
+        headers={"Authorization": f"Bearer {jwt_token}"},
+    )
+
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Only admin can create a new user"
+    user = test_db.execute(
+        text("SELECT * FROM users where username = 'test_user_3'")
+    ).fetchone()
+    assert user is None
